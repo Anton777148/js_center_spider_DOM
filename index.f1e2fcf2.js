83cc70170d51d0ed8435e8a97946c069f2d8e6a4
Anton777148 <anton777148@gmail.com>
2024-10-30 19:55:30 +0200

    make build

---
+++ index.f1e2fcf2.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=t.getBoundingClientRect();t.style.left="".concat((c.width-n.width)/2,"px"),t.style.top="".concat((c.height-n.height)/2,"px");
+//# sourceMappingURL=index.f1e2fcf2.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallSize = wall.getBoundingClientRect();
+const spiderSize = spider.getBoundingClientRect();
+spider.style.left = `${(wallSize.width - spiderSize.width) / 2}px`;
+spider.style.top = `${(wallSize.height - spiderSize.height) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
